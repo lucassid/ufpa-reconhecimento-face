8d7d561315d98067eded067e3263e532607b64af
Lucas Pacheco <lucassidpacheco@gmail.com>
2016-03-10 23:44:33 -0300

    huashuuahsuhaus

--- modelos/urf_pca.m
+++ modelos/urf_pca.m
@@ -1,4 +1,4 @@
-function PCA = urf_pca(X, num_componentes)
+function model = urf_pca(X, num_componentes)
   % faz a análise de PCA nos vetores em X
   %
   % espera:
@@ -27,14 +27,14 @@ function PCA = urf_pca(X, num_componentes)
   C= (Fi*Fi');	%dá a matriz de covariância de Fi
 
 % análise de pca através de decomposição em valores singulares (svd)
-  [E,D,V] = svd(C ,'econ');
+  [E,D,V] = svd(X ,'econ');
   
 % construção de model
   model.name = 'pca';
   model.D = diag(D);
-  model.D = model.D(1:num_components);
-  model.W = E(:,1:num_components);
-  model.num_componentes = num_components;
+  model.D = model.D(1:num_componentes);
+  model.W = E(:,1:num_componentes);
+  model.num_componentes = num_componentes;
   model.media = media;
 
 end
